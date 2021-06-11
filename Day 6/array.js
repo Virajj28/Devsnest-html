0cd7ab0eca4d3cee091287557a1e71432f92f3dd
Viraj <71481611+Virajj28@users.noreply.github.com>
2021-06-11 23:10:19 +0530

    update 2 & 4

--- Day 6/array.js
+++ Day 6/array.js
@@ -6,3 +6,21 @@
         return Array.isArray(input);
     }
 
+//2
+    console.log(array_Clone([1, 2, 4, 0]));
+    console.log(array_Clone([1, 2, [4, 0]]));
+
+    function array_Clone(input){
+        return input.slice(1,3)
+    }
+
+//3
+
+
+//4
+    myColor = ["Red", "Green", "White", "Black"];
+    console.log(myColor.join(","))
+    console.log(myColor.join(" "))
+
+//5
+
