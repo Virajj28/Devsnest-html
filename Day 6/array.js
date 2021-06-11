58e5fe032b7d37f1545ee802de04bdaced84442c
Viraj <71481611+Virajj28@users.noreply.github.com>
2021-06-11 23:46:10 +0530

    update 3rd

--- Day 6/array.js
+++ Day 6/array.js
@@ -15,7 +15,22 @@
     }
 
 //3
+    console.log(first([7, 9, 0, -2]));
+    console.log(first([],3));
+    console.log(first([7, 9, 0, -2],3));
+    console.log(first([7, 9, 0, -2],6));
+    console.log(first([7, 9, 0, -2],-3));
 
+    function first(i,n){
+        if(i==null)
+            return 0;
+        if(n==null)
+            return i[0];
+        if(n<0)
+            return [];
+        return i.slice(0,n)
+    }
+       
 
 //4
     myColor = ["Red", "Green", "White", "Black"];
@@ -23,4 +38,6 @@
     console.log(myColor.join(" "))
 
 //5
+    var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+
 
