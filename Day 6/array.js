e60542aff71e79020ad77b5ea7ad3d033bd627f8
Viraj <71481611+Virajj28@users.noreply.github.com>
2021-06-12 11:20:22 +0530

    done

--- Day 6/array.js
+++ Day 6/array.js
@@ -35,9 +35,25 @@
 //4
     myColor = ["Red", "Green", "White", "Black"];
     console.log(myColor.join(","))
-    console.log(myColor.join(" "))
+    console.log(myColor.join("+"))
 
 //5
     var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
-
-
+    var a = 1;
+    var b = 0;
+    var item;
+    for (var i=0; i<arr1.length; i++)
+    {
+            for (var j=i; j<arr1.length; j++)
+            {
+                    if (arr1[i] == arr1[j])
+                    b++;
+                    if (a<b)
+                    {
+                    a=b; 
+                    item = arr1[i];
+                    }
+            }
+            b=0;
+    }
+    console.log(item+" ( " +a +" times ) ") ;
