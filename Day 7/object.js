e47ce372439caadeef3e75b827cb23fb04eccaf6
Viraj <71481611+Virajj28@users.noreply.github.com>
2021-06-16 12:48:34 +0530

    done day 7

--- Day 7/object.js
+++ Day 7/object.js
@@ -20,7 +20,6 @@
             { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, 
             { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }
         ]; 
-
     library.forEach(item => console.log(item));
 
 //5. Write a JavaScript program to get the volume of a Cylinder with four decimal places using object classes.      
@@ -33,6 +32,29 @@
         }
     };
     console.log(dim.vol());
-    
+
 //6. Write a JavaScript program to sort an array of JavaScript objects.
-        
+var library = [ 
+    { 
+        title: 'The Road Ahead', 
+        author: 'Bill Gates', 
+        libraryID: 1254 
+    }, 
+    { 
+        title: 'Walter Isaacson', 
+        author: 'Steve Jobs', 
+        libraryID: 4264 
+    }, 
+    { 
+        title: 'Mockingjay: The Final Book of The Hunger Games', 
+        author: 'Suzanne Collins', 
+        libraryID: 3245 
+    }
+]; 
+
+    console.log(
+        library.sort (function (a,b)
+        {
+            return a.libraryID - b.libraryID;
+        })
+    )
