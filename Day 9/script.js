2fbb117a753c31b2d3084c7d7e74fb9d99ed8923
Viraj <71481611+Virajj28@users.noreply.github.com>
2021-06-19 10:37:23 +0530

    done js

---
+++ Day 9/script.js
@@ -0,0 +1,13 @@
+let seats = 0;
+const rectangles = document.querySelectorAll(".box");
+Array.from(rectangles).forEach((box) => {
+  box.addEventListener("click", (x) => {
+    let result = box.classList.toggle("active");
+    console.log(result)
+    if (result) seats++;
+    else seats--;
+    document.querySelector(".bs").innerHTML = "Booked Seats : " + seats;
+    const remaining = (document.querySelector(".rs").innerHTML =
+      "Remaining Seats : " + (36 - seats));
+  });
+});
