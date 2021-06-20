343885c170b30169ce3e757009ca9b31555b54b0
Viraj <71481611+Virajj28@users.noreply.github.com>
2021-06-20 12:32:20 +0530

    fix styles.css

--- Day 9/script.js
+++ Day 9/script.js
@@ -1,13 +1,21 @@
 let seats = 0;
+
 const rectangles = document.querySelectorAll(".square");
+
 Array.from(rectangles).forEach((square) => {
+
   square.addEventListener("click", (x) => {
+
     let result = square.classList.toggle("active");
     console.log(result)
+
     if (result) seats++;
+
     else seats--;
     document.querySelector(".he").innerHTML = "Booked Seats : " + seats;
+
     const remaining = (document.querySelector(".she").innerHTML =
       "Remaining Seats : " + (36 - seats));
-  });
+  
+    });
 });
