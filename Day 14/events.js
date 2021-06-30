49c68e61ab2ddf89578d77118f83dc1d03e3769e
Viraj <71481611+Virajj28@users.noreply.github.com>
2021-06-30 22:02:00 +0530

    done eventlisteners

--- Day 14/events.js
+++ Day 14/events.js
@@ -4,3 +4,32 @@ document.getElementById("myBtn").addEventListener("click", displayDate);
 function displayDate() {
   document.getElementById("date").innerHTML = Date();
 }
+
+
+// dbl click
+function dblclick() {
+  document.getElementById("2clicks").innerHTML = "Please check someone double clicked me!";
+}
+
+//Drag events
+/* Events fired on the drag target */
+function dragStart(event) {
+  event.dataTransfer.setData("Text", event.target.id);
+}
+
+function dragging(event) {
+  document.getElementById("drbp").innerHTML = "The element is being dragged";
+}
+
+
+/* Events fired on the drop target */
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("Text");
+  event.target.appendChild(document.getElementById(data));
+  document.getElementById("drbp").innerHTML = "The element was dropped";
+}
