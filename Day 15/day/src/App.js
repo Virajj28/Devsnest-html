40a98f6620ff680942dc06e19fe4e501538688dd
Viraj <71481611+Virajj28@users.noreply.github.com>
2021-07-06 04:17:56 +0530

    renamed

---
+++ Day 15/day/src/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import './App.css';
+import {Component} from "react";
+
+class App extends Component {
+  render() {
+    return <h1>Nervebody is Here 😎</h1>;
+  }
+}
+
+export default App;
