905b8d5e96d1d1b036a10a29044e13647388a705
Viraj <71481611+Virajj28@users.noreply.github.com>
2021-07-15 17:50:37 +0530

    done day19

--- Day19/counter/src/App.js
+++ Day19/counter/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count1: 0,
+      count2: 0,
+      count3: 0,
+      count4: 0,
+    };
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="buttons">
+          <button
+            onClick={() => {
+              this.setState({ count1: this.state.count1 + 1 });
+            }}
+          >
+            {this.state.count1}{" "}
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ count2: this.state.count2 + 1 });
+            }}
+          >
+            {this.state.count2}
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ count3: this.state.count3 + 1 });
+            }}
+          >
+            {this.state.count3}
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ count4: this.state.count4 + 1 });
+            }}
+          >
+            {this.state.count4}
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
